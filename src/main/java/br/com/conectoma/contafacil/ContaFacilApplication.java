fb5ca67e8267b269a3649bc9f6abc4fce966a490
Neirivon Elias Cardoso <neirivon@gmail.com>
2017-10-12 15:59:41 -0300

    Projeto criado

---
+++ src/main/java/br/com/conectoma/contafacil/ContaFacilApplication.java
@@ -0,0 +1,12 @@
+package br.com.conectoma.contafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaFacilApplication.class, args);
+	}
+}
