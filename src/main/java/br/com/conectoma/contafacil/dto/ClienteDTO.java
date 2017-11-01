a40b9697c57795ca3bb05c311a827bd65145353b
Neirivon Elias Cardoso <neirivon@gmail.com>
2017-11-01 14:55:33 -0200

    PUT, DELETE e GET para Cliente

---
+++ src/main/java/br/com/conectoma/contafacil/dto/ClienteDTO.java
@@ -0,0 +1,61 @@
+package br.com.conectoma.contafacil.dto;
+
+import java.io.Serializable;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import br.com.conectoma.contafacil.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	@NotEmpty(message = "O Campo NOME é de preenchimento obrigatório")
+	@Length(min=5,max=120,message="O tamanho deve ser entre 5 e 120 caracteres")
+	private String nome;
+
+	@NotEmpty(message = "O Campo EMAIL é de preenchimento obrigatório")
+	@Email(message = "Email inválido!")
+	private String email;
+	
+	public ClienteDTO() {
+		
+	}
+	
+	public ClienteDTO(Cliente obj) {
+		
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+	
+}
--- src/main/java/br/com/conectoma/contafacil/services/ClienteService.java
+++ src/main/java/br/com/conectoma/contafacil/services/ClienteService.java
@@ -1,10 +1,18 @@
 package br.com.conectoma.contafacil.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.conectoma.contafacil.domain.Cliente;
+import br.com.conectoma.contafacil.dto.ClienteDTO;
 import br.com.conectoma.contafacil.repositories.ClienteRepository;
+import br.com.conectoma.contafacil.services.exceptions.DataIntegrityException;
 import br.com.conectoma.contafacil.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,6 +28,50 @@ public class ClienteService {
 					+ ", Tipo: " + Cliente.class.getName());
 		}
 		return obj;
+	}
+	
+	
+	public Cliente update(Cliente obj) {
+
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		
+	}
+	
+	public void delete(Long id) {
+		
+		find(id); //Caso o id não exista dispara uma exceção
+		
+		try {
+			repo.delete(id);
+		} 
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas");
+		}
+		
+	}
+
+	public List<Cliente> findAll() {
+		
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
