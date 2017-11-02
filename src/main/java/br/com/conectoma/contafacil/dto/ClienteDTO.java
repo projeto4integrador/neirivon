cc76c0d8d2d204170b81fe96f26ff03b76ef6d34
Neirivon Elias Cardoso <neirivon@gmail.com>
2017-11-02 10:54:00 -0200

    Validacao customizada: email nao repetido na atualizacao de Cliente

--- src/main/java/br/com/conectoma/contafacil/dto/ClienteDTO.java
+++ src/main/java/br/com/conectoma/contafacil/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import br.com.conectoma.contafacil.domain.Cliente;
+import br.com.conectoma.contafacil.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
